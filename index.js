8afa49800551b9f23afeac36091b9ddf86132625
AronPelgrim <aron.pelgrim@hva.nl>
2022-03-31 15:25:32 +0200

    Added json storage via fs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
 
@@ -9,8 +11,26 @@ app.set('views', './views/pages')
 
 app.use(express.static('public'))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('', (req, res) => {
     res.render('index')
+})
+
+
+let userInput;
+
+app.post('/', (req, res) => {
+	console.log(req.body)
+
+	userInput = JSON.stringify(req.body.inputvalue)
+
+	fs.writeFile('informatie.json', userInput, 'utf8', cb => {
+	})
+	res.render('index', {
+		gebruikersnaam: userInput
+	})
 })
 
 app.listen(port, () => {
