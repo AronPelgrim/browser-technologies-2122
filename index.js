8eb8a5586582671f9959e8fc7faaee180f391d2f
AronPelgrim <aron.pelgrim@hva.nl>
2022-04-07 13:30:57 +0200

    Added stuff for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const fs = require('fs')
